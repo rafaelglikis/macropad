573e8a53afcce6de6ffbf74ec574e6675fa8fc4a
rafaelglikis <rafaelglikis@gmail.com>
2025-02-07 22:17:37 +0200

    Change profiles location

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source venv/bin/activate
-find . -name "*.yml" | entr -r python main.py listen profile.yml
+find . -name "*.yml" | entr -r python main.py listen prifile/profile.yml
